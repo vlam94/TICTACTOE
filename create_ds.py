7a55ffda6592bd0261feea23d0a8e871348d3e05
Rabico <vlam94@gmail.com>
2023-05-07 15:55:22 -0300

    ds created

--- create_ds.py
+++ create_ds.py
@@ -1,12 +1,11 @@
 from game import GAMEOVER,TIE, Rand_TicTacToe
-import json
 import os
-import pickle
+import pickle as pickle_rick
 
 ds_path = 'ttt_ds.pkl'
 if os.path.getsize(ds_path) > 13:
-    print ("\nWHOA! It seems you've already created the data_set!\nContinuing will fuck it UP\n\n ABORTING!\n")
-    #exit()
+    print ("\nWHOA! It seems you've already created the data_set!\nContinuing will overwrite it\n\n ABORTING!\n")
+    exit()
 
 x=0
 o=0
@@ -15,10 +14,11 @@ max_streak=0
 streak = 0
 #with open(ds_path, mode = 'w') as f:
      #f.write('[')
-num_wins = 20
+num_wins = 33000
 logs=[]
 while x < num_wins:
-      
+    if x // 1000 == 0:
+        print('still processing...')  
     try:
         game = Rand_TicTacToe()
         game.play('X')
@@ -56,6 +56,6 @@ while x < num_wins:
 
 print (f"The DummAI has won {x} times\nThe HorseAI has won {o} times\nand there has been {t} ties\nMax Horse Streak was {max_streak} wins in a row\n\n{x+o+t} matches were played\n")
 with open(ds_path, "wb") as f:
-    pickle.dump(logs, f)
+    pickle_rick.dump(logs, f)
 
     
